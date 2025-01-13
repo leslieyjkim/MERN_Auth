471ad7a1e76bb793e0b7c71964a65d9f653e5ef8
yoonjookim <yjblossom@gmail.com>
2025-01-13 14:37:29 -0500

    Create userModel to store the user data in mongoDB

---
+++ server(server_backend)/models/userModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+//To store user data in mongoDB database
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    verifyOtp: {type: String, default: ''},
+    verifyOtpExpireAt: {type: Number, default: 0},
+    isAccountVerified: {type: Boolean, default: false},
+    resetOtp: {type: String, default: ''},
+    resetOtpExpireAt: {type: Number, default: 0},
+})
+
+const userModel = mongoose.models.user || mongoose.model('user', userSchema)
+//if this userModel is available this userModel will use first option, if this's not available then it will creat this user model using userSchema.
+
+export default userModel;
