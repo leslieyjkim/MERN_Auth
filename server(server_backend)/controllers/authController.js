53ff8c1bbb26dd543624ed784c5a2410ac8a2ac3
yoonjookim <yjblossom@gmail.com>
2025-01-13 16:43:07 -0500

    Setup User logout controller function

--- server(server_backend)/controllers/authController.js
+++ server(server_backend)/controllers/authController.js
@@ -82,7 +82,26 @@ export const login = async (req, res)=>{
         return res.json({success:true}); //user successfully logged in. 
 
 
-        
+
+    } catch (error) {
+        return res.json({success:false, message: error.message})
+    }
+}
+
+
+
+
+export const logout = async (req, res)=>{
+    try {
+
+        res.clearCookie('token', {  //remove that name of token
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', 
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+
+        return res.json({success:true, message: "Logged Out"})
+
     } catch (error) {
         return res.json({success:false, message: error.message})
     }
