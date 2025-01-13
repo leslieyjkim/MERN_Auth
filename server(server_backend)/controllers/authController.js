a00d91da267212f655724b74deb6397a932a0dc6
yoonjookim <yjblossom@gmail.com>
2025-01-13 16:38:59 -0500

    Setup User login controller function

--- server(server_backend)/controllers/authController.js
+++ server(server_backend)/controllers/authController.js
@@ -34,8 +34,56 @@ export const register = async (req, res)=>{
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000  // 7 days converted in miliseconds
         })
+
+        return res.json({success:true}); //successfull registered. 
         
     } catch (error) {
         res.json({success: false, message: error.message})
+    }
+}
+
+
+
+
+
+export const login = async (req, res)=>{
+    const {email, password} = req.body;
+
+    //let's validate email & password
+    if(!email || !password){
+        return res.json({success:false, message: 'Email and Password are required'})
+    }
+
+    try {
+
+        const user = await userModel.findOne({email}); //find the user w/ specific that email
+
+        if(!user){
+            return res.json({success:false, message: 'Invalid email'})
+        }
+
+        //check password 
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.json({success:false, message: 'Invalid password'})
+        }
+
+        //generate one token to login
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        //send this token in the response 
+        res.cookie('token', token, { //cookie name is 'token', and value is token
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', //As we already put 'development' in .env file, so this line would be false.
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000  // 7 days converted in miliseconds for cookie
+        });
+
+        return res.json({success:true}); //user successfully logged in. 
+
+
+        
+    } catch (error) {
+        return res.json({success:false, message: error.message})
     }
 }
