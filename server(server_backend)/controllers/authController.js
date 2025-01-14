81e5e69e264ae435c6099f0df94d73b9562b03dc
yoonjookim <yjblossom@gmail.com>
2025-01-14 14:32:43 -0500

    Setup Verification OTP  to user's email controller function

--- server(server_backend)/controllers/authController.js
+++ server(server_backend)/controllers/authController.js
@@ -117,3 +117,38 @@ export const logout = async (req, res)=>{
         return res.json({success:false, message: error.message})
     }
 }
+
+
+// Send Verification OTP to the User's Email
+export const sendVerifyOtp = async (req, res)=>{
+    try {
+
+        const {userId} = req.body;
+
+        const user = await userModel.findById(userId);
+
+        if(user.isAccountVerified){
+            return res.json({success:false, message: "Account Already Verified"})
+        }
+        //then if the account is not verified => generate one OTP will be sent to user's email
+        const otp = String(Math.floor(100000 + Math.random() * 900000)); //generate 6 digits numbers and convert into string
+
+        //save into database
+        user.verifyOtp = otp;
+        user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000   //added 1 day expired date
+        await user.save();
+
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: 'Account Verification OTP',
+            text: `Your OTP is ${otp}. Verify your account using this OTP.`
+        }
+        await transporter.sendMail(mailOptions);
+
+        res.json({success: true, message: 'Verification OTP Sent on Email'});
+
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+} 
