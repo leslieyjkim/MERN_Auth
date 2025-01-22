fb5f140aac3275346e238ecdd34a190324d96f68
yoonjookim <yjblossom@gmail.com>
2025-01-22 14:08:20 -0500

    Setup AppContext to connect with backend 'client' folder

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Login = () => {
 
   const navigate = useNavigate()
+
+  const {backendUrl, setIsLoggedin} = useContext(AppContext)
 
   const [state, setState] = useState('Sign Up') //initial value is sign up
   const [name, setName] = useState('')
