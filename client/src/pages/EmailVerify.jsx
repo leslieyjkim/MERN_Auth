bf9f045eac438f410b963a2a34f5020de369bc95
yoonjookim <yjblossom@gmail.com>
2025-01-23 14:17:55 -0500

    Setup Emailverify page(2) setting copy-paste 6 digits at once

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -17,13 +17,23 @@ const EmailVerify = () => {
     }
   }
 
+  const handlePaste = (e) => {//Copy & paste 6 digits OTP at once
+    const paste = e.clipboardData.getData('text')
+    const pasteArray = paste.split('');
+    pasteArray.forEach((char, index)=>{  //put the splitted 6 numbers into each fields.
+      if(inputRefs.current[index]){
+        inputRefs.current[index].value = char;
+      }
+    })
+  }
+
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-amber-200'>
       <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
       <form className='bg-amber-300 p-8 rounded-lg shadow-lg w-107 text-sm'>
         <h1 className='text-blue-500 text-2xl font-semibold text-center mb-4'>Email Verify OTP</h1>
         <p className='text-center mb-6 text-blue-500'>Enter the 6-digit code sent to your email id.</p>
-        <div className='flex justify-between mb-8'>
+        <div className='flex justify-between mb-8' onPaste={handlePaste}>
           {Array(6).fill(0).map((_, index)=>(
             <input type="text" maxLength='1' key={index} required className='w-12 h-12 bg-blue-500 text-white text-center text-xl rounded-md '
             ref={e => inputRefs.current[index] = e}
