2e369dabab0a32077ecdd8838a9d243e80122f3c
yoonjookim <yjblossom@gmail.com>
2025-01-23 14:00:32 -0500

    Setup Emailverify page(2) setting OTP input fields cursor moves automatically

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const EmailVerify = () => {
+
+  const inputRefs = React.useRef([]) 
+  const handleInput = (e, index) => {
+    if(e.target.value.length > 0 && index < inputRefs.current.length -1) { //if anything was already written in input value
+      inputRefs.current[index + 1].focus();//cursor will move to the next input field automatically.
+    }  
+  }
+
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-amber-200'>
       <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
@@ -10,7 +18,10 @@ const EmailVerify = () => {
         <p className='text-center mb-6 text-blue-500'>Enter the 6-digit code sent to your email id.</p>
         <div className='flex justify-between mb-8'>
           {Array(6).fill(0).map((_, index)=>(
-            <input type="text" maxLength='1' key={index} required className='w-12 h-12 bg-blue-500 text-white text-center text-xl rounded-md '/>
+            <input type="text" maxLength='1' key={index} required className='w-12 h-12 bg-blue-500 text-white text-center text-xl rounded-md '
+            ref={e => inputRefs.current[index] = e}
+            onInput = {(e) => handleInput(e, index)}
+            />
           ))}
         </div>
         <button className='w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full'>Verify Email</button>
